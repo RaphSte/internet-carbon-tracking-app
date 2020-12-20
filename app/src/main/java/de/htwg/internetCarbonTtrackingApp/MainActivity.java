89b113bee9e07374d2870de95adb86afcde6a144
raphste <raphael.sterk@johner-institut.de>
2020-12-20 13:59:10 +0100

    #update db handling

--- app/src/main/java/de/htwg/internetCarbonTtrackingApp/MainActivity.java
+++ app/src/main/java/de/htwg/internetCarbonTtrackingApp/MainActivity.java
@@ -1,6 +1,9 @@
 package de.htwg.internetCarbonTtrackingApp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -9,6 +12,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import de.htwg.internetCarbonTtrackingApp.ui.vpn.CarbonTrackingVpnService;
 
 public class MainActivity extends AppCompatActivity {
 
